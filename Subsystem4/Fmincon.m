80e13b8e5880436ade68f99dd7dc1dc5eff6bdfc
VisakanMathy <visakan.mathivannan@emotech.co>
2019-12-11 23:29:12 +0000

    Vis subsystem first input spray mechanism

---
+++ Subsystem4/Fmincon.m
@@ -0,0 +1,21 @@
+close all
+objectiveFn = @objective;
+x0=[20,7,0.0025,0.05,0.05,6];
+
+
+A = [];
+b=[];
+Aeq = [];
+beq = [];
+lb=[0,3,0.0015,0.02,0,2];
+ub = [90,10,0.003,0.2,0.1,20];
+nonlincon = @nlcon;
+
+options = optimoptions('fmincon','Display','iter','Algorithm','interior-point');
+
+[x, fval]=fmincon(objectiveFn,x0,A,b,Aeq,beq,lb,ub,nonlincon,options)
+problem = createOptimProblem('fmincon','x0',x0,'objective',objectiveFn,'nonlcon',nonlincon,'lb',lb,'ub',ub);
+x = run(GlobalSearch,problem)
+[x, fval]=fmincon(objectiveFn,x,A,b,Aeq,beq,lb,ub,nonlincon,options)
+
+
---
+++ Subsystem4/objectivePlot.m
@@ -0,0 +1,30 @@
+function omega = objectivePlot(x)
+    rho = 997;
+    I = (1/12)*0.85*0.5*0.5;
+    
+    omega(1) = 0;
+    t(1) = 0;
+    alpha(1) = 0;
+    f_total(1) = 0;
+    j=1;
+    Kn = 1;
+    while sum(omega)==0 || round(omega(j),5) ~= round(omega(j-1),5)
+        total = 0;
+        for i = 1:x(2)
+            force = Kn*(rho*pi/(2*x(2)))*(((x(6))*x(3))^2-omega(j)*x(6)*x(3)^3);
+            distance = (x(4)*(i-1)+x(5));
+            total = (force-omega(j)*0.05)*distance;
+        end
+        f_total(j+1) = total/x(2);
+        alpha(j+1) = 2*total*I;
+        omega(j+1) = omega(j)+alpha(j+1)*t(j);
+        t(j+1)= t(j)+1;
+        j=j+1;
+    end
+    figure
+    plot(t,f_total);
+    figure
+    plot(t,omega);
+    figure
+    plot(t,alpha);
+    
