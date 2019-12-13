27de0a01109fad7930d4fe3f184f42a7a627910f
VisakanMathy <visakan.mathivannan@emotech.co>
2019-12-13 12:20:43 +0000

    Rename file to main and section the main file

--- Subsystem4/SprayMechanismMain.m
+++ Subsystem4/SprayMechanismMain.m
@@ -1,5 +1,7 @@
 close all
 clear all
+%% Inital Problem Exploration 
+%This was used to initially define the problem and explore local minimas
 objectiveFn = @objective;
 x0=[20,8,0.0025,0.05,0.05,6];
 
@@ -10,7 +12,13 @@ beq = [];
 lb=[0,4,0.0015,0.02,0,2];
 ub = [90,10,0.003,0.2,0.1,20];
 nonlincon = @nlcon;
+[x]=fmincon(objectiveFn,x,A,b,Aeq,beq,lb,ub,nonlincon);
+objectivePlot(x);
+nonlincon(x)
 
+%% Running a Global search
+%Using global search to check the local minima found by Interior Point is
+%a global minima
 problem = createOptimProblem('fmincon','x0',x0,'objective',objectiveFn,'nonlcon',nonlincon,'lb',lb,'ub',ub);
 x = run(GlobalSearch,problem);
 options = optimoptions('fmincon','Display','iter','Algorithm','sqp');
@@ -19,8 +27,10 @@ objectivePlot(x);
 x_optimal = x;
 
 %% Number of holes with fixed integers
+%Fixing the number of holes by imposing a more active boundary on lower and
+%uppe bounds.
 for i = 4:10
-    x0=[50,8,0.0025,0.05,0.05,6];
+    x0=[50,i,0.0025,0.05,0.05,6];
     lb=[0,i,0.0015,0.02,0,2];
     ub = [90,i,0.003,0.2,0.1,20];
     numberOfHoles(i-3)=i;
@@ -38,6 +48,9 @@ ylabel('Angular velocity (rad/s)')
 
 
 %% Testing Non Gradient methods
+%Using randi to entirely randomise starting points before running gradient
+%based methods.
+%note runs variable can be changed for bigger sample size
 ip = [0,0];
 sqp = [0,0];
 as = [0,0];
@@ -48,13 +61,13 @@ lb=[0,4,0.0015,0.02,0,2];
 ub = [90,4,0.003,0.2,0.1,20];
 tic;
 options = optimoptions('fmincon','Display','iter','Algorithm','interior-point');
-[x, fval]=fmincon(objectiveFn,x0,A,b,Aeq,beq,lb,ub,nonlincon,options)
+[x, fval]=fmincon(objectiveFn,x0,A,b,Aeq,beq,lb,ub,nonlincon,options);
 a = toc;
 ip(1) = ip(1) + a;
 ip(2) = ip(2) + fval;
 tic
 options = optimoptions('fmincon','Display','iter','Algorithm','sqp');
-[x, fval]=fmincon(objectiveFn,x0,A,b,Aeq,beq,lb,ub,nonlincon,options)
+[x, fval]=fmincon(objectiveFn,x0,A,b,Aeq,beq,lb,ub,nonlincon,options);
 a = toc;
 sqp(1) = sqp(1) + a;
 sqp(2) = sqp(2) + fval;
@@ -69,6 +82,9 @@ ip/runs;
 sqp/runs;
 as/runs;
 %% Post global search algorithm values
+%Using randi to entirely randomise starting points bfor a global search and
+%then use the output x values for gradient based search methods
+%note runs variable can be changed for bigger sample size
 lb=[0,4,0.0015,0.02,0,2];
 ub = [90,4,0.003,0.2,0.1,20];
 ip = [0,0];
@@ -102,6 +118,8 @@ ip/runs;
 sqp/runs;
 as/runs;
 %% Sensitivity analysis of speed
+%Using Fixed value of number of holes x(2) and varying speed of jet x(6)
+%using lb and ub.
 options = optimoptions('fmincon','Display','iter','Algorithm','sqp');
 for j = 2:19
     x = x_optimal;
@@ -118,6 +136,8 @@ xlabel('Jet velocity (m/s)')
 ylabel('Angular velocity (rad/s)')
 
 %% Sensitivity analysis of radius of hole
+%Using fixed value of x(2) and varying sizes of nozzles x(3) using lb and
+%ub
 options = optimoptions('fmincon','Display','iter','Algorithm','sqp');
 for j = 1:15
     x=x_optimal;
@@ -135,6 +155,7 @@ ylabel('Angular velocity (rad/s)')
 
 
 %% Sensitivity analysis of angles
+%Using fixed value of x(2) and varying angle of jet x(1)
 options = optimoptions('fmincon','Display','iter','Algorithm','sqp');
 for j = 1:10
     x=x_optimal;
